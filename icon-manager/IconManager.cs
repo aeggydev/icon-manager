503f27861810420241ca1b81a9e845079430dfcc
aeggydev <kucernak.a@gmail.com>
2020-12-19 18:51:56 +0100

    Added support for creating Icons from other image files

--- icon-manager/IconManager.cs
+++ icon-manager/IconManager.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using ImageMagick;
 
 namespace icon_manager
 {
@@ -29,6 +30,51 @@ namespace icon_manager
 
     public class Icon
     {
+        public string Path { get; }
+        public FileInfo Info { get; }
+
+        private Icon(string path)
+        {
+            Path = path;
+            Info = new FileInfo(path);
+        }
+
+        public static Icon FromPath(string path)
+        {
+            var exists = File.Exists(path);
+            if (!exists)
+            {
+                throw new FileNotFoundException($"File doesn't exist {path}");
+            }
+
+            if (!System.IO.Path.GetExtension(path).Equals(".ico"))
+            {
+                throw new FileFormatException($"File is not .ico {path}");
+            }
+
+            return new Icon(path);
+        }
+
+        public static Icon CreateFromImage(string path)
+        {
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException($"File doesn't exist {path}");
+            }
+
+            var file = new FileInfo(path);
+            var icoPath = System.IO.Path.Combine(file.Directory.FullName, "icon.ico");
+
+            // TODO: Add checking for image shape
+            // TODO: Add checking of input image format
+            using (var image = new MagickImage(path))
+            {
+                image.Scale(256, 256);
+                image.Write(icoPath);
+            }
+
+            return new Icon(icoPath);
+        }
     }
 
     public class Ini

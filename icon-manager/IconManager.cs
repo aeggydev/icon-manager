4fa0f358020cfb2f21f9d16b8b7728da67037b4f
aeggydev <kucernak.a@gmail.com>
2020-12-19 17:21:57 +0100

    Add basic properties and constructor to Directory

--- icon-manager/IconManager.cs
+++ icon-manager/IconManager.cs
@@ -1,17 +1,37 @@
-﻿namespace icon_manager
+﻿using System.IO;
+
+namespace icon_manager
 {
     public class Directory
     {
-        
+        public string Path { get; }
+        public FileInfo FileInfo { get; }
+        public DirectoryInfo DirInfo { get; }
+
+        public Directory(string path)
+        {
+            Path = path;
+            FileInfo = new FileInfo(path);
+            DirInfo = new DirectoryInfo(path);
+        }
+
+        public static Directory FromPath(string path)
+        {
+            var exists = System.IO.Directory.Exists(path);
+            if (!exists)
+            {
+                throw new DirectoryNotFoundException($"Path doesn't exist {path}");
+            }
+
+            return new Directory(path);
+        }
     }
 
     public class Icon
     {
-        
     }
 
     public class Ini
     {
-        
     }
 }
